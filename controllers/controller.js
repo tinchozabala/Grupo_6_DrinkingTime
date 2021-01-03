283f7de9eb59ef7d9cbdfad47a578e9f5e54c3d4
facundoferrero <f_ferrero@hotmail.com>
2021-01-03 20:07:06 -0300

    migrando las funciones de ruta al archivo de controlador

---
+++ controllers/controller.js
@@ -0,0 +1,7 @@
+let controlador = {
+    index : ( req, res) => {
+        res.render('index')
+    }
+}
+
+module.exports = controlador
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const productosControlador = require ('../controllers/controller')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index')
-});
+router.get('/', productosControlador.index);
 
 module.exports = router;
