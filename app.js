99e2ae7e9af84d5373e93ef26dcf6ba917b08e86
rojasmcamilo <cristhianrojas.95@live.com>
2021-01-04 22:57:32 -0300

    arreglo de archivos

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride("_method"));
 
 app.use('/products', productsRouter);
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
