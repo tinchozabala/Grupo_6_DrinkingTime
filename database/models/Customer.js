02762cee70c2dfef852948d043b7136d94f1d00a
rojasmcamilo <cristhianrojas.95@live.com>
2021-02-03 20:28:35 -0300

    creamos el primer modelo de la db Customer

---
+++ database/models/Customer.js
@@ -0,0 +1,54 @@
+const sequelize = require("sequelize");
+
+module.exports = (sequelize, dataTypes) => {
+    // EL ALIAS VA EN PLURAL 
+    let alias = "Customers"
+    // DEFINIR COLUMNAS - NO  DEBE FALTAR EL TYPE
+    let cols = {
+        id : {
+            autoIncrement: true,
+            primaryKey: true, 
+            type: dataTypes.INTEGER,
+            allowNull: false
+        },
+        email: {
+            type: dataTypes.STRING,
+            allowNull: false
+        }, 
+        name: {
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: dataTypes.STRING,
+            allowNull: false
+        }, 
+        shipping_addres: {
+            type: dataTypes.STRING
+        },
+        avatar: {
+            type: dataTypes.STRING
+        },
+        telephone:{
+            type: dataTypes.STRING
+        },
+        birth_date: {
+            type: dataTypes.DATE
+        }, 
+        created_at:{
+            type: dataTypes.DATE
+        }, 
+        updated_at: {
+            type: dataTypes.DATE
+        },
+        deleted_at: {
+            type: dataTypes.DATE
+        }
+    }
+
+    // No agregamos config, de timestamps
+
+    const Customer = sequelize.define(alias, cols, config)
+
+    return Customer
+}
