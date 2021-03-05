00783fa93197db17d01563694028af24e49f607b
rojasmcamilo <cristhianrojas.95@live.com>
2021-03-05 10:47:18 -0300

    Se crea validacion formulario de registro

--- public/js/register.js
+++ public/js/register.js
@@ -8,19 +8,47 @@ window.addEventListener("load", function(){
         }else{
             errorsfront.innerHTML = ""
         }
-        console.log(rgemail.test(email.value))
     })
 
     const errorsfrontpass = document.querySelector(".errorsfrontpass")
     const rgpassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
 
-    let contraseña = document.querySelector("#password")
-    contraseña.addEventListener("blur", ()=>{
+    let contraseña = document.querySelector("#contraseña")
+    contraseña.addEventListener("keyup", ()=>{
         if (rgpassword.test(contraseña.value) == false){
-            errorsfrontpass.innerHTML = "Este campo es obligatorio"
+            errorsfrontpass.innerHTML = "La contraseña debe contener 8 digitos entre: mayusculas, minusculas y numeros "
         }else{
             errorsfrontpass.innerHTML = ""
         }
     })
 
+    const errorsfrontrepass = document.querySelector(".errorsfrontrepass")
+    let confirmPass = document.querySelector("#confirmPass")
+    confirmPass.addEventListener("keyup", ()=>{
+        if (contraseña.value != confirmPass.value){
+            errorsfrontrepass.innerHTML = "Las contraseñas no coinciden"
+        } else{
+            errorsfrontrepass.innerHTML = ""
+        }
+    })
+
+    const errorsfrontname = document.querySelector(".errorsfrontname")
+    let name = document.querySelector("#name")
+    name.addEventListener("keyup", () =>{
+        let namevalue = name.value
+        if ( namevalue.lenght <= 2) {
+            errorsfrontname.innerHTML = "Debe tener al menos 2 caracteres"
+        }else{
+            errorsfrontname.innerHTML = ""
+        }
+    })
+
+   // const form = document.querySelector(".formulario-login")
+   // form.addEventListener("sumbit", (e) =>{
+   //     if(revisa.value == "Revisa tus datos"){
+   //         e.preventDefault()
+   //     }else{
+   //
+   //     }        
+   // })
 })
