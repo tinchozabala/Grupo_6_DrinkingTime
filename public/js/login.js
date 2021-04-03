db16f9607e26dc839d1357618544fcc56b983e56
rojasmcamilo <cristhianrojas.95@live.com>
2021-04-03 20:55:06 -0300

    Se trabaja sobre el js del login

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,34 @@
 window.addEventListener("load", function(){
     const errorsfront = document.querySelector(".errorsfront")
-    const rgemail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
-    let email = document.querySelector("#email");
-    email.addEventListener("blur", ()=>{
-        if (email.value == ""){
-            errorsfront.innerHTML = "Este campo es obligatorio"
-        }else{
-            errorsfront.innerHTML = ""
-        }
-        console.log(rgemail.test(email.value))
+        const rgemail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+        let email = document.querySelector("#email");
+        email.addEventListener("blur", ()=>{
+            if (email.value == ""){
+                errorsfront.innerHTML = "Este campo es obligatorio"
+            }else{
+                errorsfront.innerHTML = ""
+            }
+            console.log(rgemail.test(email.value))
+        })
+    
+        const errorsfrontpass = document.querySelector(".errorsfrontpass")
+        let contraseña = document.querySelector("#password")
+        contraseña.addEventListener("blur", ()=>{
+            if (contraseña.value == ""){
+                errorsfrontpass.innerHTML = "Este campo es obligatorio"
+            }else{
+                errorsfrontpass.innerHTML = ""
+            }
+        })
+
+    const login = document.querySelector(".formulario-login")
+    login.addEventListener("submit", function(e){
+    if(errorsfront && errorsfrontpass == ""){
+        login.submit()        
+    }
+
     })
 
-    const errorsfrontpass = document.querySelector(".errorsfrontpass")
-    let contraseña = document.querySelector("#password")
-    contraseña.addEventListener("blur", ()=>{
-        if (contraseña.value == ""){
-            errorsfrontpass.innerHTML = "Este campo es obligatorio"
-        }else{
-            errorsfrontpass.innerHTML = ""
-        }
-    })
 
     const viewpass = document.querySelector("#view-pass")
     viewpass.addEventListener("click", ()=>{
@@ -29,5 +38,7 @@ window.addEventListener("load", function(){
             contraseña.setAttribute("type", "password")
         }
     })
+
+    
 
 })
