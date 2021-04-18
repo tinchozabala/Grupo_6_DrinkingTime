b095c92668a61186a23851c8ecfeb05ceedb6c65
rojasmcamilo <cristhianrojas.95@live.com>
2021-04-18 18:03:51 -0500

    Confirmacion de eliminacion de producto

--- public/js/deleteProduct.js
+++ public/js/deleteProduct.js
@@ -1,26 +1,20 @@
-var swal = require('sweetalert')
 window.addEventListener("load", ()=>{
-    const deleteProduct = document.querySelectorAll("#deleteProduct")
-    console.log(deleteProduct);
-    for (let i = 0; i<deleteProduct.length; i ++){
-        deleteProduct[i].addEventListener("click", ()=>{
-            swal({
-                title: "Are you sure?",
-                text: "Once deleted, you will not be able to recover this imaginary file!",
-                icon: "warning",
-                buttons: true,
-                dangerMode: true,
-              })
-              .then((willDelete) => {
-                if (willDelete) {
-                  swal("Poof! Your imaginary file has been deleted!", {
-                    icon: "success",
-                  });
-                } else {
-                  swal("Your imaginary file is safe!");
-                }
-              });
 
-        })
+    let deleteProduct = document.querySelectorAll("#deleteProduct");
+    console.log(deleteProduct);
+
+    for (let i = 0; i<deleteProduct.length; i++){
+
+      deleteProduct[i].addEventListener('click', function(event){
+
+        let confirmacion = confirm("¿Desea eliminar el producto?")
+        if (confirmacion === false){
+          event.preventDefault()
+        }else{
+          deleteProduct.submit()
+        }
+      })
     }
+    
+
 })
---
+++ public/js/sweetAlert.js
@@ -0,0 +1,8 @@
+const { default: Swal } = require("sweetalert2");
+
+window.addEventListener("load", ()=>{
+   Swal.fire(
+       title: "error"
+   )
+
+})
